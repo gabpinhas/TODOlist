2129811c003528359d98dc1e1c95e306b8119f5b
JamesMakarov <thiagonogueira@alu.ufc.br>
2024-04-15 20:57:16 -0300

    Decouple

--- todolist/settings.py
+++ todolist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a#uun4xcidi74vozt9eg)7%d9(q7z1x2*^%jx1+u3#%yj&!ta@'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default = False, cast = bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default = [], cast = Csv())
 
 # Application definition
 
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'todolist.urls'
+ROOT_URLCONF = config('ROOT_URLCONF')
 
 TEMPLATES = [
     {
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Fortaleza'
 
 USE_I18N = True
 
@@ -128,9 +128,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587  # Porta do servidor SMTP
-EMAIL_USE_TLS = True  # Use TLS (True) ou SSL (False) para conexão segura
-EMAIL_HOST_USER = 'todotoyoulist@gmail.com'  # Seu endereço de e-mail remetente
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')  # Porta do servidor SMTP
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default = False, cast = bool)  # Use TLS (True) ou SSL (False) para conexão segura
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')  # Seu endereço de e-mail remetente
 EMAIL_HOST_PASSWORD = ''  # Sua senha de e-mail remetente
